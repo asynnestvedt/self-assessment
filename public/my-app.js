923cfa3bd1682176def39890ba44254447ce5b78
Alan Synnestvedt <alan.synnestvedt@centene.com>
2023-12-23 01:15:56 +0000

    simplify routing

--- public/my-app.js
+++ public/my-app.js
@@ -7,43 +7,30 @@ class MyApp extends HTMLElement {
 
     connectedCallback() {
         fetch('/api/alldata')
-                .then(r => r.json())
-                .then(d => {
-                    window.facets = d.facets
-                    window._data = d
-                })
+            .then(r => r.json())
+            .then(d => {
+                window.facets = d.facets
+                window._data = d
+            })
 
-        window.addEventListener('load',()=>{
-            function initHashChangeListener() {
-                function onHashChange() {
-                    const hash = window.location.hash
-                    const path = hash.substring(1)
-                    const route = parseRoute(path)
-                    handleRouteChange(route)
-                }
-                function parseRoute(path) {
-                    return path.split('/')
-                }
-                function handleRouteChange(route) {
-                    const contentContainer = document.querySelector('my-container')
-                    contentContainer.innerHTML = ''
+        window.addEventListener('hashchange', this.onRoute)
+        this.onRoute()
+    }
+    
+    onRoute() {
+        const route = window.location.hash.substring(1).split('/')[0]
+        const contentContainer = document.querySelector('my-container')
+        contentContainer.innerHTML = ''
 
-                    if (route[0] === '') {
-                        contentContainer.innerHTML = '<my-welcome></my-welcome>'
-                    } else if (route[0] === 'assessment' || route[0] === '') {
-                        contentContainer.innerHTML = '<assessment-page></assessment-page>'
-                    } else if (route[0] === 'about') {
-                        contentContainer.innerHTML = '<h1>About Page</h1>'
-                    } else {
-                        contentContainer.innerHTML = '<h1>404 Not Found</h1>'
-                    }
-                }
-                window.addEventListener('hashchange', onHashChange)
-                onHashChange()
-            }
-
-            initHashChangeListener()
-        })
+        if (route === '') {
+            contentContainer.innerHTML = '<my-welcome></my-welcome>'
+        } else if (route === 'assessment' || route === '') {
+            contentContainer.innerHTML = '<assessment-page></assessment-page>'
+        } else if (route === 'about') {
+            contentContainer.innerHTML = '<h1>About Page</h1>'
+        } else {
+            contentContainer.innerHTML = '<h1>404 Not Found</h1>'
+        }
     }
 
     static template() {
